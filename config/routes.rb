646f8e1a06af10c99cc0b40706e980b77c01e3bc
Masahito Osako <masahito.osako@hey.com>
2023-11-12 23:40:23 +0900

    管理画面用の記事APIのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :articles, param: :handle, only: %i[index show]
 
+  namespace :admin do
+    resources :articles, param: :handle, except: %i[new edit]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
