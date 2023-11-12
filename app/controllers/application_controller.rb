bdb6eb08694df6ae651e88b4b49cad174974e597
Masahito Osako <masahito.osako@hey.com>
2023-11-12 22:10:02 +0900

    add verify_token method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :verify_token
+
+  private
+
+  def verify_token
+    authenticate_or_request_with_http_token do |token|
+      ActiveSupport::SecurityUtils.secure_compare(token, Rails.env.fetch['API_TOKEN'])
+    end
+  end
 end
